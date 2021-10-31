1c638ad89119268ebbac9597f7eb29781e8f7cc0
Xin Wang <xw2519@ic.ac.uk>
2021-10-31 14:25:01 +0000

    Readme added

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-import matplotlib
-from bin.decision_tree import create_decision_tree, predict_dataset
+from bin.decision_tree import create_decision_tree, predict_dataset, width
 from bin.evaluation import calculate_confusion_matrix, calculate_evaluation_metrics, cross_validation
 from bin.util import read_and_shuffle_dataset, plot_decision_tree, plot_confusion_matrix, print_evaluation_metrics, print_cross_validation_metrics
-from bin.decision_tree import width
 
-from sklearn import tree
-import numpy
-import sys
-numpy.set_printoptions(threshold=sys.maxsize)
+import numpy as np
+
 '''
 app.py
 
@@ -15,6 +11,56 @@ Main python program accessible to the user and links all subprograms together
 '''
 
 if __name__ == "__main__":
+    while True:
+        # User prompt
+        while True:
+            filepath = input('\nInput path to dataset: ')
+            try:
+                loaded_dataset = np.loadtxt(filepath)
+                break
+            except:
+                print('Path not valid! Try again')
+        print()
+        print('Program functions: [Please enter the corresponding number]')
+        selected_function = input(' 1: Plot Decision Tree \n 2: 10-fold cross validation \n 3: Nested 10-fold cross validation with post-pruning \n \nChoice: ')
+        print()
+        
+        # Function execution
+        if (selected_function == '1'):
+            print("-------------------- Program execution log --------------------")
+            print("1: Plot Decision Tree")
+            print()
+            print("Loading dataset from: ", filepath)
+            training_set, training_label = read_and_shuffle_dataset(filepath)
+            print("Plotting decision tree")
+            decision_tree_model, max_tree_depth = create_decision_tree(training_dataset = training_set, label = training_label, tree_depth = 0)  
+            plot_decision_tree(decision_tree_model, width)
+            print("Plotting completed. Plot saved to 'output/decision_tree_model.jpg'")
+            print() 
+            input("To select other functions, press 'Enter'")
+        
+        elif (selected_function == '2'):
+            print("-------------------- Program execution log --------------------")
+            print("2: 10-fold cross validation")
+            print()
+            print("Loading dataset from: ", filepath)
+            print()
+            average_accuracy, average_precision, average_recall, average_f1_score, average_confusion_matrix = cross_validation(filepath)
+            print_cross_validation_metrics(average_accuracy, average_precision, average_recall, average_f1_score, average_confusion_matrix)
+            print("Plotting confusion matrix")
+            plot_confusion_matrix(average_confusion_matrix)
+            print("Plotting completed. Plot saved to 'output/confusion_matrix.jpg'")
+            print() 
+            input("To select other functions, press 'Enter'")
+                    
+        elif (selected_function == '3'):
+            print("-------------------- Program execution log --------------------")
+            print("3: Nested 10-fold cross validation with post-pruning")
+            
+            print() 
+            input("To select other functions, press 'Enter'")
+            ...
+            
     # Load and shuffle data
     dataset, label = read_and_shuffle_dataset("./wifi_db/clean_dataset.txt")
     
