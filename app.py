bc0c7c091f7772ccb42679b2890cfa0906ca7a6a
Yousijun <sy1819@ic.ac.uk>
2021-11-04 19:42:28 +0000

    add comment & tidy up

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+"""
+app.py
+
+Main python program accessible to the user and links all subprograms together.
+Allow users to choose the function they want to operate.
+"""
 from bin.decision_tree import create_decision_tree, width
 from bin.evaluation import cross_validation
 from bin.util import read_and_shuffle_dataset, plot_confusion_matrix, print_cross_validation_metrics, plot_decision_tree, plot_decision_tree_v2
@@ -8,12 +14,6 @@ import numpy
 import sys
 
 numpy.set_printoptions(threshold=sys.maxsize)
-
-'''
-app.py
-
-Main python program accessible to the user and links all subprograms together
-'''
 
 if __name__ == "__main__":
     while True:
@@ -31,20 +31,23 @@ if __name__ == "__main__":
         print()
         
         # Function execution
+        # Plot decision tree
         if (selected_function == '1'):
             print("-------------------- Program execution log --------------------")
             print("1: Plot Decision Tree")
             print()
             print("Loading dataset from: ", filepath)
             training_set, training_label = read_and_shuffle_dataset(filepath)
-            print("Plotting variations of decision trees")
+            print("Plotting variations of decision trees...")
             decision_tree_model, max_tree_depth = create_decision_tree(training_dataset = training_set, label = training_label, tree_depth = 0)  
             max_tree_depth += 1
             plot_decision_tree(decision_tree_model, max_tree_depth, './output/decision_tree_model.jpg')
             plot_decision_tree_v2(decision_tree_model, width, './output/decision_tree_model_v2.jpg')
             print() 
-            input("To select other functions, press 'Enter'")
-        
+            user_input = input("To exit, type 'q'; Continue otherwise : ")
+            if (user_input == 'q') : break
+            else : continue
+        # Run 10-fold cross validation
         elif (selected_function == '2'):
             print("-------------------- Program execution log --------------------")
             print("2: 10-fold cross validation")
@@ -55,12 +58,14 @@ if __name__ == "__main__":
             print()
             average_accuracy, accuracy_standard_deviation, average_precision, average_recall, average_f1_score, average_confusion_matrix = cross_validation(filepath, seed)
             print_cross_validation_metrics(average_accuracy, accuracy_standard_deviation, average_precision, average_recall, average_f1_score, average_confusion_matrix)
-            print("Plotting confusion matrix")
+            print("Plotting confusion matrix...")
             plot_confusion_matrix(average_confusion_matrix)
             print("Plotting completed. Plot saved to 'output/confusion_matrix.jpg'")
             print() 
-            input("To select other functions, press 'Enter'")
-                    
+            user_input = input("To exit, type 'q'; Continue otherwise : ")
+            if (user_input == 'q') : break
+            else : continue
+        # Run tested 10-fold cross validation with post-pruning            
         elif (selected_function == '3'):
             print("-------------------- Program execution log --------------------")
             print("3: Nested 10-fold cross validation with post-pruning")
@@ -81,10 +86,12 @@ if __name__ == "__main__":
             print_cross_validation_metrics(pruned_accuracies, pruned_accuracy_standard_deviation ,pruned_average_precision, pruned_average_recall, pruned_average_f1_score, pruned_average_confusion_matrix, True, True)
             print("----------------------------------------------------------------------------------------------------------------")
             print()
-            print("Plotting confusion matrix")
+            print("Plotting confusion matrix...")
             plot_confusion_matrix(pruned_average_confusion_matrix)
             print("Plotting completed. Plot saved to 'output/confusion_matrix.jpg'")
             print()
-            input("To select other functions, press 'Enter'")
+            user_input = input("To exit, type 'q'; Continue otherwise : ")
+            if (user_input == 'q') : break
+            else : continue
             
             
--- bin/decision_tree.py
+++ bin/decision_tree.py
@@ -1,11 +1,14 @@
+"""
+decision_tree.py
+    
+Contain functions which create the decision tree and do the prediction:
+    create_decision_tree(training_dataset, label, tree_depth)
+    predict_dataset(test_dataset, decision_tree_model)
+    predict_single_instance(test_instance, decision_tree_model)
+
+"""
 from bin.util import find_split
 import numpy as np    
-
-'''
-tree.py
-    
-Creates and manipulates the decision tree
-'''
 
 # Global variable declaration 
 tree_depth = 0
@@ -13,8 +16,8 @@ width = [0,0,0,0,0]
 
 # Decision tree functions
 def create_decision_tree(training_dataset, label, tree_depth):
-    '''
-    Given a training dataset, labels and initial tree depth, create the decision tree
+    """
+    Given a training dataset, labels and initial tree depth, create the decision tree.
     
     Parameters:
     - training_dataset: Array of values to train the decision tree 
@@ -22,11 +25,12 @@ def create_decision_tree(training_dataset, label, tree_depth):
     - tree_depth: Value indicating the current depth of the tree
     
     Return:
-    - node: A decision tree model in dictionary form
+    - node: A decision tree model with type dictionary
     - max_depth: The maximum depth of the decision tree model
-    '''
+    """
     [classes, unique_label] = np.unique(label, return_inverse=True) 
     
+    # when the examples correspond to only one class - reach the leaf node
     if(len(classes) == 1):
         leaf_node = {"class" : classes[0], "leaf" : True}
 
@@ -34,27 +38,35 @@ def create_decision_tree(training_dataset, label, tree_depth):
         else: width[tree_depth] += 1
         
         return leaf_node, tree_depth
+    # recurse until reach the leaf node
     else:
+        # find the attribute and value that result in the highest information gain
         attribute, value, cut_point = find_split(training_dataset, label) 
 
+        # sort the dataset
         order = np.argsort(training_dataset[:, attribute])
         data = training_dataset[order]
         label = label[order]
         
+        # split the dataset based on the split point given that result in the highest information gain
         l_dataset=data[:cut_point] 
         l_label=label[:cut_point]
         
         r_dataset=data[cut_point:]
         r_label=label[cut_point:]
 
+        # create node
         node = {"attribute" : attribute, "value" : value, "left" : {} , "right" : {}, "leaf" : False}
         
+        # count tree_depth
         if tree_depth >= len(width): width.append(1)
         else: width[tree_depth] += 1
         
+        # recursion
         l_branch, l_depth = create_decision_tree(l_dataset, l_label, tree_depth + 1)
         r_branch, r_depth = create_decision_tree(r_dataset, r_label, tree_depth + 1)
 
+        # add nodes to the decision tree
         node["left"] = l_branch
         node["right"] = r_branch
 
@@ -62,7 +74,7 @@ def create_decision_tree(training_dataset, label, tree_depth):
 
 
 def predict_dataset(test_dataset, decision_tree_model):
-    '''
+    """
     Given a test dataset and trained decision tree model, predict the appropriate labels of the test dataset
     
     Parameters:
@@ -71,12 +83,12 @@ def predict_dataset(test_dataset, decision_tree_model):
     
     Return:
     - predicted_labels: Array of predicted labels of 'test_dataset'
-    '''
+    """
     return [ predict_single_instance(test_dataset[i, :], decision_tree_model) for i in range(len(test_dataset)) ]
 
 
 def predict_single_instance(test_instance, decision_tree_model):
-    '''
+    """
     Given a single test instance and trained decision tree model, predict the appropriate label of that single test instance
     
     Parameters:
@@ -85,14 +97,16 @@ def predict_single_instance(test_instance, decision_tree_model):
     
     Return:
     predicted_labels: Predicted label of 'test_instance'
-    '''
+    """
+    # If "leaf" node is reached, prediction is complete and return the predicted class
     if (decision_tree_model["leaf"] == True): 
-        # If "leaf" node is reached, prediction is complete and return the predicted class
         return int(decision_tree_model["class"])
+    # If "leaf" node is not reached, traverse the decision tree
     else: 
-        # If "leaf" node is not reached, traverse the decision tree
-        if test_instance[ decision_tree_model["attribute"] ] <= decision_tree_model["value"]: return predict_single_instance(test_instance, decision_tree_model["left"])
-        else: return predict_single_instance(test_instance, decision_tree_model["right"])
+        if test_instance[ decision_tree_model["attribute"] ] <= decision_tree_model["value"]: 
+            return predict_single_instance(test_instance, decision_tree_model["left"])
+        else: 
+            return predict_single_instance(test_instance, decision_tree_model["right"])
 
 
 
